4694512d7af4ac1b2702dc8d7fb61ce6515fbaa1
alialratha2012-cpu <alialratha2012@gmail.com>
2026-06-12 19:13:25 +0300

    Add bot code

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const { Client } = require("discord.js");
 
+const client = new Client({ intents: [] });
+
+client.once("ready", () => {
+  console.log("Bot Online");
+});
+
+client.login(process.env.TOKEN);
